ee8f173ed5191fb85825223b31d540379133b7d7
Arnau Mora <arnyminerz@proton.me>
2024-01-01 20:42:46 +0100

    Added `isInsecure` and `isValid`
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/src/main/java/at/bitfire/icsdroid/model/CredentialsModel.kt
+++ app/src/main/java/at/bitfire/icsdroid/model/CredentialsModel.kt
@@ -1,5 +1,6 @@
 package at.bitfire.icsdroid.model
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
@@ -7,4 +8,19 @@ class CredentialsModel : ViewModel() {
     val requiresAuth = MutableLiveData(false)
     val username = MutableLiveData("")
     val password = MutableLiveData("")
+
+    val isInsecure = MutableLiveData(false)
+
+    val isValid = MediatorLiveData<Boolean>().apply {
+        fun update() {
+            value = if (requiresAuth.value == true)
+                !username.value.isNullOrEmpty() && !password.value.isNullOrEmpty()
+            else
+                true
+        }
+
+        addSource(requiresAuth) { update() }
+        addSource(username) { update() }
+        addSource(password) { update() }
+    }
 }
