4afc0ef650ccde16958d657a084008c8a1dec7a8
Arnau Mora <arnyminerz@proton.me>
2024-01-03 13:13:26 +0100

    Renamed tests
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/src/androidTest/java/at/bitfire/icsdroid/ui/ValidationModelTestTest.kt
+++ app/src/androidTest/java/at/bitfire/icsdroid/ui/ValidationModelTestTest.kt
@@ -20,7 +20,7 @@ import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 
-class AddCalendarEnterUrlFragmentValidationTest {
+class ValidationModelTestTest {
 
     companion object {
 
