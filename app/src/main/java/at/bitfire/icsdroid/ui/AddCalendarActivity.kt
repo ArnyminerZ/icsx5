31fcf2a2e7271edd3b02a135d2acc57427a65733
Arnau Mora <arnyminerz@proton.me>
2024-01-11 10:21:39 +0100

    Migrated EnterUrlFragment to Activity
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/java/at/bitfire/icsdroid/ui/AddCalendarActivity.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/AddCalendarActivity.kt
@@ -4,13 +4,55 @@
 
 package at.bitfire.icsdroid.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.core.content.ContextCompat
+import at.bitfire.icsdroid.Constants
+import at.bitfire.icsdroid.HttpClient
+import at.bitfire.icsdroid.HttpUtils
+import at.bitfire.icsdroid.R
 import at.bitfire.icsdroid.calendar.LocalCalendar
+import at.bitfire.icsdroid.model.CredentialsModel
 import at.bitfire.icsdroid.model.SubscriptionSettingsModel
+import at.bitfire.icsdroid.model.ValidationModel
+import com.google.accompanist.themeadapter.material.MdcTheme
+import kotlinx.coroutines.launch
+import okhttp3.HttpUrl.Companion.toHttpUrl
+import java.net.URI
+import java.net.URISyntaxException
 
-class AddCalendarActivity: AppCompatActivity() {
+@OptIn(ExperimentalFoundationApi::class)
+class AddCalendarActivity : AppCompatActivity() {
 
     companion object {
         const val EXTRA_TITLE = "title"
@@ -18,19 +60,27 @@ class AddCalendarActivity: AppCompatActivity() {
     }
 
     private val subscriptionSettingsModel by viewModels<SubscriptionSettingsModel>()
+    private val credentialsModel by viewModels<CredentialsModel>()
+    private val validationModel by viewModels<ValidationModel>()
+
+    private val pickFile =
+        registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri: Uri? ->
+            if (uri != null) {
+                // keep the picked file accessible after the first sync and reboots
+                contentResolver.takePersistableUriPermission(
+                    uri,
+                    Intent.FLAG_GRANT_READ_URI_PERMISSION
+                )
+
+                subscriptionSettingsModel.url.value = uri.toString()
+            }
+        }
 
 
     override fun onCreate(inState: Bundle?) {
         super.onCreate(inState)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
         if (inState == null) {
-            supportFragmentManager
-                    .beginTransaction()
-                    .add(android.R.id.content, AddCalendarEnterUrlFragment())
-                    .commit()
-
             intent?.apply {
                 data?.let { uri ->
                     subscriptionSettingsModel.url.value = uri.toString()
@@ -39,9 +89,237 @@ class AddCalendarActivity: AppCompatActivity() {
                     subscriptionSettingsModel.title.value = it
                 }
                 if (hasExtra(EXTRA_COLOR))
-                    subscriptionSettingsModel.color.value = getIntExtra(EXTRA_COLOR, LocalCalendar.DEFAULT_COLOR)
+                    subscriptionSettingsModel.color.value =
+                        getIntExtra(EXTRA_COLOR, LocalCalendar.DEFAULT_COLOR)
             }
         }
+
+        setContent {
+            MdcTheme {
+                val scope = rememberCoroutineScope()
+                val pagerState = rememberPagerState { 2 }
+
+                val url: String? by subscriptionSettingsModel.url.observeAsState(null)
+                val urlError: String? by subscriptionSettingsModel.urlError.observeAsState(null)
+                val supportsAuthentication: Boolean by subscriptionSettingsModel.supportsAuthentication.observeAsState(false)
+                val requiresAuth: Boolean by credentialsModel.requiresAuth.observeAsState(false)
+                val username: String? by credentialsModel.username.observeAsState(null)
+                val password: String? by credentialsModel.password.observeAsState(null)
+                val isInsecure: Boolean by credentialsModel.isInsecure.observeAsState(false)
+                val isVerifyingUrl: Boolean by validationModel.isVerifyingUrl.observeAsState(false)
+                val validationResult: ResourceInfo? by validationModel.result.observeAsState(null)
+
+                var showNextButton by remember { mutableStateOf(false) }
+
+                LaunchedEffect(url, requiresAuth, username, password, isVerifyingUrl) {
+                    if (isVerifyingUrl) {
+                        showNextButton = true
+                        return@LaunchedEffect
+                    }
+
+                    val uri = validateUri()
+                    val authOK =
+                        if (requiresAuth)
+                            !username.isNullOrEmpty() && !password.isNullOrEmpty()
+                        else
+                            true
+                    showNextButton = uri != null && authOK
+                }
+
+                LaunchedEffect(validationResult) {
+                    Log.i("AddCalendarActivity", "Validation result updated: $validationResult")
+                    if (validationResult == null || validationResult?.exception != null) return@LaunchedEffect
+                    val info = validationResult!!
+
+                    // When a result has been obtained, and it's neither null nor has an exception,
+                    // clean the subscriptionSettingsModel, and move the pager to the next page
+                    subscriptionSettingsModel.url.value = info.uri.toString()
+
+                    if (subscriptionSettingsModel.color.value == null)
+                        subscriptionSettingsModel.color.value =
+                            info.calendarColor ?: ContextCompat.getColor(this@AddCalendarActivity, R.color.lightblue)
+
+                    if (subscriptionSettingsModel.title.value.isNullOrBlank())
+                        subscriptionSettingsModel.title.value = info.calendarName ?: info.uri.toString()
+
+                    scope.launch { pagerState.animateScrollToPage(pagerState.currentPage + 1) }
+                }
+
+                Scaffold(
+                    topBar = { TopAppBar(pagerState, showNextButton) }
+                ) { paddingValues ->
+                    HorizontalPager(
+                        state = pagerState,
+                        userScrollEnabled = false,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues)
+                    ) { page ->
+                        when (page) {
+                            0 -> EnterUrlComposable(
+                                requiresAuth = requiresAuth,
+                                onRequiresAuthChange = credentialsModel.requiresAuth::setValue,
+                                username = username,
+                                onUsernameChange = credentialsModel.username::setValue,
+                                password = password,
+                                onPasswordChange = credentialsModel.password::setValue,
+                                isInsecure = isInsecure,
+                                url = url,
+                                onUrlChange = subscriptionSettingsModel.url::setValue,
+                                urlError = urlError,
+                                supportsAuthentication = supportsAuthentication,
+                                isVerifyingUrl = isVerifyingUrl,
+                                validationResult = validationResult,
+                                onValidationResultDismiss = { validationModel.result.value = null },
+                                onPickFileRequested = { pickFile.launch(arrayOf("text/calendar")) },
+                                onSubmit = { onNextRequested(1) }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        HttpClient.setForeground(false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        HttpClient.setForeground(true)
+    }
+
+
+    @Composable
+    private fun TopAppBar(pagerState: PagerState, showNextButton: Boolean) {
+        val scope = rememberCoroutineScope()
+        androidx.compose.material.TopAppBar(
+            navigationIcon = {
+                IconButton(
+                    onClick = {
+                        // If first page, close activity
+                        if (pagerState.currentPage <= 0) finish()
+                        // otherwise, go back a page
+                        else scope.launch { pagerState.animateScrollToPage(pagerState.currentPage - 1) }
+                    }
+                ) {
+                    Icon(Icons.Filled.ArrowBack, null)
+                }
+            },
+            title = { Text(text = stringResource(R.string.activity_add_calendar)) },
+            actions = {
+                AnimatedVisibility(visible = showNextButton) {
+                    IconButton(onClick = { onNextRequested(pagerState.currentPage) }) {
+                        Icon(Icons.Filled.ArrowForward, null)
+                    }
+                }
+            }
+        )
+    }
+
+
+    private fun onNextRequested(page: Int) {
+        when (page) {
+            // First page (Enter Url)
+            0 -> {
+                // flush the credentials if auth toggle is disabled
+                if (credentialsModel.requiresAuth.value != true) {
+                    credentialsModel.username.value = null
+                    credentialsModel.password.value = null
+                }
+
+                val uri: Uri? = subscriptionSettingsModel.url.value?.let(Uri::parse)
+                val authenticate = credentialsModel.requiresAuth.value ?: false
+
+                if (uri != null) {
+                    validationModel.validate(
+                        uri,
+                        if (authenticate) credentialsModel.username.value else null,
+                        if (authenticate) credentialsModel.password.value else null
+                    )
+                }
+            }
+            // Second page (details and confirm)
+            1 -> {
+                // TODO
+            }
+        }
+    }
+
+
+    /* dynamic changes */
+
+    private fun validateUri(): Uri? {
+        var errorMsg: String? = null
+
+        var uri: Uri
+        try {
+            try {
+                uri = Uri.parse(subscriptionSettingsModel.url.value ?: return null)
+            } catch (e: URISyntaxException) {
+                Log.d(Constants.TAG, "Invalid URL", e)
+                errorMsg = e.localizedMessage
+                return null
+            }
+
+            Log.i(Constants.TAG, uri.toString())
+
+            if (uri.scheme.equals("webcal", true)) {
+                uri = uri.buildUpon().scheme("http").build()
+                subscriptionSettingsModel.url.value = uri.toString()
+                return null
+            } else if (uri.scheme.equals("webcals", true)) {
+                uri = uri.buildUpon().scheme("https").build()
+                subscriptionSettingsModel.url.value = uri.toString()
+                return null
+            }
+
+            val supportsAuthenticate = HttpUtils.supportsAuthentication(uri)
+            subscriptionSettingsModel.supportsAuthentication.value = supportsAuthenticate
+            when (uri.scheme?.lowercase()) {
+                "content" -> {
+                    // SAF file, no need for auth
+                }
+
+                "http", "https" -> {
+                    // check whether the URL is valid
+                    try {
+                        uri.toString().toHttpUrl()
+                    } catch (e: IllegalArgumentException) {
+                        Log.w(Constants.TAG, "Invalid URI", e)
+                        errorMsg = e.localizedMessage
+                        return null
+                    }
+
+                    // extract user name and password from URL
+                    uri.userInfo?.let { userInfo ->
+                        val credentials = userInfo.split(':')
+                        credentialsModel.requiresAuth.value = true
+                        credentialsModel.username.value = credentials.elementAtOrNull(0)
+                        credentialsModel.password.value = credentials.elementAtOrNull(1)
+
+                        val urlWithoutPassword =
+                            URI(uri.scheme, null, uri.host, uri.port, uri.path, uri.query, null)
+                        subscriptionSettingsModel.url.value = urlWithoutPassword.toString()
+                        return null
+                    }
+                }
+
+                else -> {
+                    errorMsg = getString(R.string.add_calendar_need_valid_uri)
+                    return null
+                }
+            }
+
+            // warn if auth. required and not using HTTPS
+            credentialsModel.isInsecure.value =
+                credentialsModel.requiresAuth.value == true && !uri.scheme.equals("https", true)
+        } finally {
+            subscriptionSettingsModel.urlError.value = errorMsg
+        }
+        return uri
     }
 
 }
