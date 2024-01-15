90a37fee389fcbf8e29bedb3f1c058f48fc09109
Arnau Mora <arnyminerz@proton.me>
2024-01-15 18:06:29 +0100

    Added theme colors
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

---
+++ app/src/main/java/at/bitfire/icsdroid/ui/theme/Colors.kt
@@ -0,0 +1,7 @@
+package at.bitfire.icsdroid.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val colorPrimary = Color(0xff039be5)
+val colorPrimaryDark = Color(0xff01579b)
+val colorSecondary = Color(0xffff2200)
