77e05b3c90c1dfb1f71de073a3dee4f7ebafd40f
Arnau Mora <arnyminerz@proton.me>
2024-01-24 16:06:18 +0100

    Added system bar color theming
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- app/src/main/java/at/bitfire/icsdroid/ui/theme/Theme.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/theme/Theme.kt
@@ -1,24 +1,101 @@
 package at.bitfire.icsdroid.ui.theme
 
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.contentColorFor
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionContext
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+
+private val LightColors = lightColors(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    secondary = md_theme_light_secondary,
+    onSecondary = md_theme_light_onSecondary,
+    error = md_theme_light_error,
+    onError = md_theme_light_onError,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface
+)
+
+private val DarkColors = darkColors(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    secondary = md_theme_dark_secondary,
+    onSecondary = md_theme_dark_onSecondary,
+    error = md_theme_dark_error,
+    onError = md_theme_dark_onError,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface
+)
 
 @Composable
-fun AppTheme(content: @Composable () -> Unit) {
+fun AppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    // TODO: Dark theme
+
+    val context = LocalContext.current
+
+    val colors = if (darkTheme)
+        DarkColors
+    else
+        LightColors
+
     MaterialTheme(
-        colors = MaterialTheme.colors.copy(
-            primary = colorPrimary,
-            primaryVariant = colorPrimaryDark,
-            secondary = colorSecondary,
-            onSecondary = Color.White
-        ),
-        content = content
-    )
+        colors = colors
+    ) {
+        LaunchedEffect(darkTheme) {
+            if (context is AppCompatActivity) {
+                val style = if (darkTheme)
+                    SystemBarStyle.dark(
+                        md_theme_dark_primaryContainer.toArgb()
+                    )
+                else
+                    SystemBarStyle.light(
+                        md_theme_light_surfaceTint.toArgb(),
+                        md_theme_light_onBackground.toArgb()
+                    )
+                context.enableEdgeToEdge(
+                    statusBarStyle = style,
+                    navigationBarStyle = style
+                )
+            } else {
+                Log.e("AppTheme", "Context is not activity!")
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                // Required to make sure all paddings are correctly set
+                .safeDrawingPadding()
+                .fillMaxSize()
+        ) {
+            content()
+        }
+    }
 }
 
 /**
@@ -38,14 +115,16 @@ fun AppTheme(content: @Composable () -> Unit) {
  * Then, applies [AppTheme] to the UI.
  *
  * @param parent The parent composition reference to coordinate scheduling of composition updates
+ * @param darkTheme Calculates whether the UI should be shown in light or dark theme.
  * @param content A `@Composable` function declaring the UI contents
  */
 fun ComponentActivity.setContentThemed(
     parent: CompositionContext? = null,
+    darkTheme: @Composable () -> Boolean = { isSystemInDarkTheme() },
     content: @Composable () -> Unit
 ) {
     setContent(parent) {
-        AppTheme {
+        AppTheme(darkTheme = darkTheme()) {
             content()
         }
     }
@@ -59,6 +138,7 @@ fun ComponentActivity.setContentThemed(
  *
  * @param content A `@Composable` function declaring the UI contents
  */
+@Deprecated("Fragments should not be used")
 fun ComposeView.setContentThemed(content: @Composable () -> Unit) {
     setContent {
         AppTheme {
