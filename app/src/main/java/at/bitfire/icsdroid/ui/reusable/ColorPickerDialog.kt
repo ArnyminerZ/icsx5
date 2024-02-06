7d143e1f9b5c6f63880072b70eecdcaf9fbdfbd5
Sunik Kupfer <kupfer@bitfire.at>
2024-02-06 11:44:03 +0100

    Remove default color button

--- app/src/main/java/at/bitfire/icsdroid/ui/reusable/ColorPickerDialog.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/reusable/ColorPickerDialog.kt
@@ -5,14 +5,7 @@
 package at.bitfire.icsdroid.ui.reusable
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.res.stringResource
-import at.bitfire.icsdroid.R
 import at.bitfire.icsdroid.calendar.LocalCalendar
-import com.maxkeppeker.sheets.core.models.base.SelectionButton
 import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
 import com.maxkeppeler.sheets.color.ColorDialog
 import com.maxkeppeler.sheets.color.models.ColorConfig
@@ -45,14 +38,11 @@ fun ColorPickerDialog(
         0xFFFFA000.toInt(),
     )
 
-    var selectedColor by remember { mutableIntStateOf(initialColor) }
     ColorDialog(
         state = rememberUseCaseState(visible = true, onCloseRequest = { onDialogDismissed() }),
         selection = ColorSelection(
-            selectedColor = SingleColor(selectedColor),
+            selectedColor = SingleColor(initialColor),
             onSelectColor = onSelectColor,
-            extraButton = SelectionButton(stringResource(R.string.add_calendar_default_color)),
-            onExtraButtonClick = { selectedColor = LocalCalendar.DEFAULT_COLOR }
         ),
         config = ColorConfig(
             templateColors = templateColors,
