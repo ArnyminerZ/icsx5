3109d0d8a39c43d90dbc44dbd14c49003a4ac8a3
Arnau Mora <arnyminerz@proton.me>
2024-02-08 14:42:24 +0100

    Migrated to Gradle Version Catalog
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,17 @@
 pluginManagement {
     repositories {
+        google()
+        mavenCentral()
         gradlePluginPortal()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
     }
 }
 
