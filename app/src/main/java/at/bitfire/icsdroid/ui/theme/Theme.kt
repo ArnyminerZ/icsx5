e9a4f0ed9170cacf9ba747a6a99839c1d036160e
Arnau Mora <arnyminerz@proton.me>
2024-02-12 11:59:24 +0100

    Using less colors
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/java/at/bitfire/icsdroid/ui/theme/Theme.kt
+++ app/src/main/java/at/bitfire/icsdroid/ui/theme/Theme.kt
@@ -29,29 +29,15 @@ import androidx.compose.ui.platform.LocalContext
 import at.bitfire.icsdroid.Settings
 
 private val LightColors = lightColors(
-    primary = md_theme_light_primary,
-    onPrimary = md_theme_light_onPrimary,
-    secondary = md_theme_light_secondary,
-    onSecondary = md_theme_light_onSecondary,
-    error = md_theme_light_error,
-    onError = md_theme_light_onError,
-    background = md_theme_light_background,
-    onBackground = md_theme_light_onBackground,
-    surface = md_theme_light_surface,
-    onSurface = md_theme_light_onSurface
+    primary = colorPrimary,
+    secondary = colorSecondary,
+    onSecondary = Color.White
 )
 
 private val DarkColors = darkColors(
-    primary = md_theme_dark_primary,
-    onPrimary = md_theme_dark_onPrimary,
-    secondary = md_theme_dark_secondary,
-    onSecondary = md_theme_dark_onSecondary,
-    error = md_theme_dark_error,
-    onError = md_theme_dark_onError,
-    background = md_theme_dark_background,
-    onBackground = md_theme_dark_onBackground,
-    surface = md_theme_dark_surface,
-    onSurface = md_theme_dark_onSurface
+    primary = colorPrimaryDark,
+    secondary = colorSecondary,
+    onSecondary = Color.White
 )
 
 @Composable
